3878b5f973d2bb8da047a984c76bd4c2e0f4c09f
Kun Ran <rankun203@gmail.com>
2019-09-19 01:23:33 +0800

    feat: add dist

---
+++ dist/utils/useSessionTimers.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const useInterval_1 = __importDefault(require("./useInterval"));
+exports.default = (session) => {
+    const { expiration, isAuthenticated, removeSession, refreshFn, refreshInterval, setSession } = session;
+    /***
+     * Remove Session Timer
+     */
+    const sessionExpiresIn = expiration && isAuthenticated ? expiration.valueOf() - Date.now() : null;
+    useInterval_1.default(() => removeSession(), sessionExpiresIn);
+    /***
+     * RefreshFn timer
+     */
+    let refreshExpiresIn = null;
+    if (refreshFn && refreshInterval) {
+        refreshExpiresIn = Math.min(refreshInterval, sessionExpiresIn || Infinity);
+    }
+    useInterval_1.default(() => {
+        const refreshed = Promise.resolve(refreshFn(session));
+        refreshed.then(_session => setSession(_session));
+    }, refreshExpiresIn);
+};
+//# sourceMappingURL=useSessionTimers.js.map
